73affa8c64de89e997c87a9387817d2acdfaa576
SomThaGod <53866232+SomThaGod@users.noreply.github.com>
2019-08-26 11:28:01 -0700

    Delete Assignment1Q1.java

--- Assignment1Q1.java
+++
@@ -1,8 +0,0 @@
-public class Output1{
-    public static void main(String[] args){
-        char ch='a';
-        System.out.println(5/2);
-        System.out.println(5/2.0);
-        System.out.println((((int)ch+1)%2)==0);
-    }
-}
